ea496451f3057a2125be0d904a73e08fb6e1d44e
sadmanislam10 <islamsadman10@gmail.com>
2026-07-10 22:30:18 +0600

    added main.c and build.sh

--- build.sh
+++ build.sh
@@ -2,13 +2,11 @@
 
 echo "Compiling Vroom..."
 
-# Compile main.c using Linux Raylib flags
-gcc main.c -O1 -Wall -std=c99 -Wno-missing-braces -o vroom -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+gcc main.c src/*.c -O1 -Wall -std=c99 -Wno-missing-braces -o game -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 
-# $? checks the exit status of the gcc command (0 means success)
 if [ $? -eq 0 ]; then
     echo "Build Successful! Launching game..."
-    ./vroom
+    ./game
 else
     echo "Build Failed! Check the terminal for errors."
 fi
---
+++ main.c
@@ -0,0 +1,28 @@
+#include <raylib.h>
+#include "src/game.h"
+
+#define WINDOW_WIDTH 1000
+#define WINDOW_HEIGHT 800
+
+int main(void)
+{
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Vroom");
+    SetTargetFPS(60);
+
+    GameState state = game_init(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    while (!WindowShouldClose())
+    {
+        float dt = GetFrameTime();
+
+        game_update(&state, dt);
+
+        BeginDrawing();
+        ClearBackground(WHITE);
+        game_draw(&state);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
