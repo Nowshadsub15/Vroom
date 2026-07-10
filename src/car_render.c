6625d68e187d751fa1d769b5f82b5c208dacdd14
sadmanislam10 <islamsadman10@gmail.com>
2026-07-10 22:17:16 +0600

    added car_render.c

---
+++ src/car_render.c
@@ -0,0 +1,23 @@
+#include "car.h"
+
+#define TRANSPARENT_BLACK (Color){0, 0, 0, 100}
+
+#define DEBUG_SUSPENSION 1
+
+void car_draw(Car *car)
+{
+    DrawRectanglePro((Rectangle){
+                         .width = car->width,
+                         .height = car->height,
+                         .x = car->position.x,
+                         .y = car->position.y,
+                     },
+                     (Vector2){car->width / 2, car->height / 2}, car->angle, TRANSPARENT_BLACK);
+    DrawCircle(car->back_wheel.position.x, car->back_wheel.position.y, car->back_wheel.radius, TRANSPARENT_BLACK);
+    DrawCircle(car->front_wheel.position.x, car->front_wheel.position.y, car->front_wheel.radius, TRANSPARENT_BLACK);
+
+#if DEBUG_SUSPENSION
+    DrawCircleV(car->back_wheel.attachment_point, 10, GREEN);
+    DrawCircleV(car->front_wheel.attachment_point, 10, GREEN);
+#endif
+}
