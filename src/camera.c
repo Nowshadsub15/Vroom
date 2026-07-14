2457e7437e15e9125356d60694985dd8cce86eea
Nowshadsub15 <sayemrahman589@gmail.com>
2026-07-14 20:33:40 +0600

    minor fix

--- src/camera.c
+++ src/camera.c
@@ -1,9 +1,9 @@
 #include "camera.h"
 #include <math.h>
 
-#define CAMERA_ZOOM_MAX 1.3f
-#define CAMERA_ZOOM_MIN 0.5f
-#define CAMERA_ZOOM_SPEED 1.0f
+#define CAMERA_ZOOM_MAX 1.3
+#define CAMERA_ZOOM_MIN 0.5
+#define CAMERA_ZOOM_SPEED 1.0
 
 Camera2D camera_init(int window_width, int window_height)
 {
@@ -27,7 +27,7 @@ void camera_update(Camera2D *camera, Vector2 target, float velocity_x, float dt)
 
     if (speed > 3.0f) 
     {
-        target_zoom = CAMERA_ZOOM_MAX - ((speed - 3.0f) / 15.0f); 
+        target_zoom = CAMERA_ZOOM_MAX - ((speed - 3.0) / 15.0); 
     }
 
     if (target_zoom > CAMERA_ZOOM_MAX) target_zoom = CAMERA_ZOOM_MAX;
